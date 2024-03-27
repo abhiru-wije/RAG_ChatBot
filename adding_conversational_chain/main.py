121806deb61ee24e8c4e8f7d3a4c2b2b21ba987b
abhiru-wije <abdwije19972219@gmail.com>
2024-03-27 17:34:53 +0530

    set main conversation method

---
+++ adding_conversational_chain/main.py
@@ -0,0 +1,40 @@
+from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from decouple import config
+
+# embedding function
+embedding_function = SentenceTransformerEmbeddings(
+    model_name="all-MiniLM-L6-v2"
+)
+
+vector_db = Chroma(
+    persist_directory= "../vector_db",
+    collection_name="megareward_collection",
+    embedding_function=embedding_function
+)
+
+llm = ChatOpenAI(openai_api_key=config("OPENAI_API_KEY"), temperature=0.5)
+
+memory = ConversationBufferMemory(
+    return_messages=True,
+    memory_key="chat_history"
+)
+
+qa_chain = ConversationalRetrievalChain.from_llm(
+    llm=llm,
+    memory=memory,
+    retriever=vector_db.as_retriever(
+        search_kwargs={"fetch": 4, "k": 3}, search_type="mmr"),
+    chain_type="refine"
+)
+
+question = "What is the book about?"
+
+response = qa_chain({"question": question})
+
+print(response.get("answer"))
